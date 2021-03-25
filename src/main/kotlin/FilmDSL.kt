ade97e15229d9943b8f634ab8d4d1722530e9bab
Maria <mari.ponosova.1997@mail.ru>
2021-03-25 04:22:14 +0300

    Add Film DSL with tests

---
+++ src/main/kotlin/FilmDSL.kt
@@ -0,0 +1,18 @@
+data class Film(
+    var name: String = "",
+    var reviews: MutableList<Review> = mutableListOf(),
+) {
+    /**
+     * Возвращает количество отзывов, у которых оценка равна [rating]
+     */
+    fun numberOfReviews(rating: Int): Int {
+        return reviews.count { it.rating == rating }
+    }
+}
+
+data class Review(
+    var rating: Int = 0,
+    var comment: String = "",
+)
+
+fun film(block: Film.() -> Unit): Film = Film().apply(block)
