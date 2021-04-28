04c8e3a8f8d62ca71e491b672a21ee0a87c6a94b
Maria <mari.ponosova.1997@mail.ru>
2021-04-28 20:16:17 +0300

    Update connection info for sqlserver

--- orders/src/main/resources/schema.sql
+++ orders/src/main/resources/schema.sql
@@ -1,3 +1,16 @@
+USE master;
+IF EXISTS(SELECT *
+          FROM sys.databases
+          WHERE name = 'orders_db')
+    BEGIN
+        ALTER DATABASE orders_db SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+        USE master
+        DROP DATABASE IF EXISTS orders_db
+    END;
+CREATE DATABASE orders_db;
+
+USE orders_db;
+
 DROP TABLE IF EXISTS "Order";
 
 CREATE TABLE "Order"
